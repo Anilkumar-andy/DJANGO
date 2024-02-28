a331211f4b8ed3dbfd6629c2beab377c7791a02c
AnilKumar Prajapati <150881412+Ak-andy@users.noreply.github.com>
2024-02-28 15:33:27 +0000

    search

--- CLass/5_search_component/Search_component/blog/urls.py
+++ CLass/5_search_component/Search_component/blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns=[
     path('',views.PostIndexView.as_view(),name='post-list'),
-    path('detail/<int:pk',views.PostDetailView.as_view(), name='post_detail'),
+    path('detail/<int:pk>,views.PostDetailView.as_view(), name='post_detail'),
     path('search-blogs/',views.BlogSearchView.as_view(), name='search_blogs'),
 ]
