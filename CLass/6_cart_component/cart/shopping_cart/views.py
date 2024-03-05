2a306d62c3086de3ac1e00b92acd9d39cc63fe7a
AnilKumar Prajapati <150881412+Ak-andy@users.noreply.github.com>
2024-03-05 15:31:19 +0000

    cart

---
+++ CLass/6_cart_component/cart/shopping_cart/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import CartItem
+# Create your views here.
+
+@login_required
+def add_to_cart(request):
+    if request.method == 'POST':
+        #Handle adding item to cart
+        product_name = request.POST.get('product_name')
+        quantity = int(request.POST.get('quantity'))
+        price = float(request.POST.get('price'))
+        #you need to handle cart managemnt here
+        CartItem.objects.create(
+            user = request.User,
+            product_name = product_name,
+            quantity=quantity,
+            price=price
+        )
+    return redirect('cart:shopping_cart_view')
+
+@login_required
+def remove_from_cart(request,item_id):
+    # Handle removing item from cart
+    cart_item= CartItem.objects.get(id= item_id)
+    cart_item.delete()
+    return redirect('cart:shopping_cart_view')
+
+@login_required
+def cart_view(request):
+    #retrieve cart items and render cart template
+    cart_items= CartItem.objects.filter(user=request.user)
+    total_price = sum(item.price *item.quantity for item in cart_items)
+    context={
+        'cart_items':cart_items,
+        'total_price':total_price
+    }
+    return render(request,'shopping/cart.html', context)
