263420b3eabad18a1507f97c5d81efad04fbbde0
AnilKumar Prajapati <150881412+Ak-andy@users.noreply.github.com>
2024-03-11 15:22:02 +0000

    crud

---
+++ CLass/7.1_crud_application/contact/contact_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path("create",views.create),
+    path("dashboard",views.dashboard),
+    path("edit/<sid>",views.edit),
+    path("delete/<did>",views.delete),
+]
