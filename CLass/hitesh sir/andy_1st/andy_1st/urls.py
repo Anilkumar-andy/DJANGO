f2ff19b315d063ce5353170dc59e004d851c51d0
AnilKumar Prajapati <150881412+Ak-andy@users.noreply.github.com>
2024-03-22 15:53:20 +0000

    update

--- CLass/hitesh sir/andy_1st/andy_1st/urls.py
+++ CLass/hitesh sir/andy_1st/andy_1st/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from views import *
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("home/",home, name="index")
+    path("home/",views.home, name="index")
 ]
