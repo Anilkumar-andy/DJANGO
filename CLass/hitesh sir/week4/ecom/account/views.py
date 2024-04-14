02e43a801e7e5b848e26b5027c7882348ec06d38
AnilKumar Prajapati <150881412+Ak-andy@users.noreply.github.com>
2024-04-14 06:15:05 +0000

    update

--- CLass/hitesh sir/week4/ecom/account/views.py
+++ CLass/hitesh sir/week4/ecom/account/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .forms import LoginForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 def login(request):
-    return render(request, "account/login.html")
+    if request.method =='GET':
+        form =LoginForm()
+        return render(request, "account/login.html",{'form':form})
+    elif request.method == 'POST':
+        form =LoginForm(request.POST)
+        username=form['username'].Value()
+        user = User.objects.get(username=username)
+        print(user)
+        return redirect('-login')
 def register(request):
     return render(request,"account/register.html")
 def logout(request):
