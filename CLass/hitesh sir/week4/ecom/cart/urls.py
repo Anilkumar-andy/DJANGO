dae8395758a19e21a78242167c4d46d48e92d96b
AnilKumar Prajapati <150881412+Ak-andy@users.noreply.github.com>
2024-04-14 07:40:14 +0000

    update

--- CLass/hitesh sir/week4/ecom/cart/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path("cart/", views.cart ,name="-cart"),
-    path("payment", views.payment, name="-payment")
-]
--- CLass/hitesh sir/week4/ecom/product/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from product.views import *
-
-urlpatterns = [
-    #path('home/',home,name='home')
-    path('',product,name='-product'),
-    path('add_product/',add_product,name="add-product"),
-    path('details/<int:id>/',show_details)
-]
