3fa70267968631c343a3faa132d106e185452b5d
AnilKumar Prajapati <150881412+Anilkumar-andy@users.noreply.github.com>
2024-05-11 04:03:54 +0000

    update

--- project/pr/RoadLink-master/repair/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Repair
-
-class RepairForm(forms.ModelForm):
-    class Meta:
-        model=Repair
-        fields=('issue','vehicle',)
--- project/pr/RoadLink-master/vehicle/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Vehicle
-
-class VehicleForm(forms.ModelForm):
-    class Meta:
-        model=Vehicle
-        fields = ('cost_per_km','price','registration_plate','no_of_km_travelled','mileage','vehicle_type','fuel_type','insurance_status','image')
